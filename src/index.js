b9e37d7f0fc9be617fffb995f645a288079bafc2
José Manuel <expressmillionaire@hotmail.com>
2018-05-05 09:16:54 +0200

    generated API key

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
+// import keys from './config/keys'
+// keys.WEATHER_INFO_KEY
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 
