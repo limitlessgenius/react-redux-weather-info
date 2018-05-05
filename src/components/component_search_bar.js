814e29bf986f8d00fdd1d28615dea82ab1d80613
José Manuel <expressmillionaire@hotmail.com>
2018-05-05 08:58:43 +0200

    form component

--- src/components/component_search_bar.js
+++ src/components/component_search_bar.js
@@ -3,12 +3,35 @@
 import React, { Component } from 'react'
 
 export default class SearchBar extends Component {
+
+	constructor(props) {
+		super(props)
+
+		this.state = {
+			term: ''
+		}
+	}
+
+	onInputChange = (e) => { 
+		this.setState({ term: e.target.value }) 
+		//WHY ERROR IF I DON'T BIND TO THIS
+	}
+
+	onFormSubmit = (e) => {
+		e.preventDefault()
+	}
+	
 	render() {
 		return (
-			<form className="input-group">
-				<input className="input-group-button" />
-				<span>
-					<button className="btn btn-secondary" type="submit">Submit</button>
+			<form className="input-group" onSubmit={this.onFormSubmit}>
+				<input 
+					placeholder="Get 5 day forecast"
+					className="form-control"
+					value={this.state.term}
+					onChange={this.onInputChange}
+				/>
+				<span className="input-group-button">
+					<button type="submit" className="btn btn-secondary">Submit</button>
 				</span>
 			</form>
 		)
