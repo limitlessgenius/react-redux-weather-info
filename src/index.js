887896e11f58443d6634c310215e6304fbc4ebfa
José Manuel <expressmillionaire@hotmail.com>
2018-05-05 09:21:31 +0200

    example call

--- src/index.js
+++ src/index.js
@@ -7,6 +7,10 @@ import App from './components/App';
 
 // import keys from './config/keys'
 // keys.WEATHER_INFO_KEY
+// EXAMPLE CALL
+// http://api.openweathermap.org/data/2.5/forecast?
+// q=Barcelona,es
+// &APPID=5308e936a98dbca642b24fcb6c118199
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
